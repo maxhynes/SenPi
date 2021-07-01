41528fc2f2d4d4f90e894e68557fc841429fc643
meepmeep22 <meepmeep22>
2021-07-01 15:23:25 -0700

    another change

--- main.py
+++ main.py
@@ -3,3 +3,4 @@ from time import sleep     # Import the sleep function from the time module
 GPIO.setwarnings(False)    # Ignore warning for now
 GPIO.setmode(GPIO.BOARD)   # Use physical pin numbering
 GPIO.setup(8, GPIO.OUT, initial=GPIO.LOW)   # Set pin 8 to be an output pin and set initial value to low (off)  
+print("hello")
